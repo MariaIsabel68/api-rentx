07fad30d610a7389e8ed712742060448c5cce97a
Maria Isabel <mioejow.gmail.com>
2021-07-08 16:54:16 -0300

    Database docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,15 +1,14 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
 
     return createConnection(
         Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === "test" ? "localhost" : host,
             database:
                 process.env.NODE_ENV === "test"
                     ? "rentx_test"
                     : defaultOptions.database,
-        }) // se for test, usa o banco de dados "", se não, usa o default
+        })
     );
 };
